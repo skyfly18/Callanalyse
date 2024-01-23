028b6825fade36d108d435455f6d2713e4c60680
Sebastian Kurth <1sebastian1@online.de>
2024-01-23 14:59:18 +0100

    Update version number

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "de.skyfly"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
--- springBootCallAnalyse/build.gradle.kts
+++ springBootCallAnalyse/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "de.skyfly"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
