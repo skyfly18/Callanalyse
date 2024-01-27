c8fd4caed460359d85108ce9e0b5a207f561da8d
Sebastian Kurth <1sebastian1@online.de>
2024-01-27 20:44:46 +0100

    comments

--- springBootCallAnalyse/src/main/java/de/skyfly/callanalyse/config/AppConfig.java
+++ springBootCallAnalyse/src/main/java/de/skyfly/callanalyse/config/AppConfig.java
@@ -4,18 +4,38 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "api")
 public class AppConfig {
-    private String indexurl;
-    private String downloadOrdnerPath;
-
+    /**
+     * Maximum number of threads to use when running the application.
+     */
     private int maxAnzahlThread = 100;
 
+    /**
+     * Download path for saving all files.
+     */
+    private String downloadOrdnerPath;
+
+    /**
+     * Flag to determine whether to build index or not.
+     */
     private Boolean buildIndex;
 
+    /**
+     * Core pool size of the thread pool.
+     */
     private int corePoolSize = 12;
 
-    private  int corePoolSizeMax = 110;
+    /**
+     * Maximum number of threads in the thread pool.
+     */
+    private int corePoolSizeMax = 110;
 
-    private  int queueCapacitySize = 300;
+    /**
+     * Capacity of the queue for threads
+     */
+    private int queueCapacitySize = 300;
+
+    /** Index URL to use for building index. */
+    private String indexurl;
 
     public int getMaxAnzahlThread() {
         return maxAnzahlThread;
@@ -26,8 +46,9 @@ public class AppConfig {
     }
 
 
-
-
+    /** Download path for saving all files.
+     * @return the downloadOrdnerPath
+     */
     public String getDownloadOrdnerPath() {
         return downloadOrdnerPath;
     }
@@ -37,44 +58,81 @@ public class AppConfig {
     }
 
 
-
+    /** Flag to determine whether to build index or not.
+     * @return the buildIndex
+     */
     public Boolean getBuildIndex() {
         return buildIndex;
     }
 
+    /**
+     * Sets the build index property.
+     * @param buildIndex the build index value
+     */
     public void setBuildIndex(Boolean buildIndex) {
         this.buildIndex = buildIndex;
     }
 
-
+    /**
+     * Returns the index URL to use for building index.
+     * @return the indexurl
+     */
     public String getIndexurl() {
         return indexurl;
     }
 
+    /**
+     * Sets the index URL to use for building index.
+     * @param indexurl the index url value
+     */
     public void setIndexurl(String indexurl) {
         this.indexurl = indexurl;
     }
 
+    /**
+     * Returns the core pool size of the thread pool.
+     * @return the corePoolSize
+     */
     public int getCorePoolSize() {
         return corePoolSize;
     }
 
+    /**
+     * Sets the core pool size of the thread pool.
+     * @param corePoolSize the core pool size value
+     */
     public void setCorePoolSize(int corePoolSize) {
         this.corePoolSize = corePoolSize;
     }
 
+    /**
+     * Returns the maximum size of the core pool.
+     * @return the corePoolSizeMax
+     */
     public int getCorePoolSizeMax() {
         return corePoolSizeMax;
     }
 
+    /**
+     * Sets the maximum size of the core pool.
+     * @param corePoolSizeMax the maximum size value
+     */
     public void setCorePoolSizeMax(int corePoolSizeMax) {
         this.corePoolSizeMax = corePoolSizeMax;
     }
 
+    /**
+     * Returns the queue capacity size of the thread pool.
+     * @return the queueCapacitySize
+     */
     public int getQueueCapacitySize() {
         return queueCapacitySize;
     }
 
+    /**
+     * Sets the queue capacity size of the thread pool.
+     * @param queueCapacitySize the queue capacity size value
+     */
     public void setQueueCapacitySize(int queueCapacitySize) {
         this.queueCapacitySize = queueCapacitySize;
     }
