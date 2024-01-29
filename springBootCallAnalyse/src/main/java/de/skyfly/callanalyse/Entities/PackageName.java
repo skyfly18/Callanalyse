4bde6e024bb425d6412ada7597797287eba7f0bf
Sebastian Kurth <1sebastian1@online.de>
2024-01-29 07:35:52 +0100

    comments

--- springBootCallAnalyse/src/main/java/de/skyfly/callanalyse/Entities/PackageName.java
+++ springBootCallAnalyse/src/main/java/de/skyfly/callanalyse/Entities/PackageName.java
@@ -10,11 +10,13 @@ import java.util.Set;
 @Entity(name = "package_name")
 @Cacheable
 public class PackageName {
+    // Primary key of the entity
     @jakarta.persistence.Id
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    // Unique identifier for this package
     @Column(unique = true, length= Length.LONG32)
     private String gaString;
 
@@ -23,7 +25,7 @@ public class PackageName {
     @OneToOne(fetch = FetchType.LAZY)
     private CalculatedResult calculatedResult;
 
-
+    // List of PackageVersions
     @OneToMany(
             cascade = CascadeType.ALL,
             fetch = FetchType.LAZY,
@@ -31,41 +33,70 @@ public class PackageName {
     )
     public Set<PackageVersion> versionsList;
 
+    // Whether or not the entity has been processed
     private Boolean processed = false;
 
 
-    public void setVersionsList(Set<PackageVersion> versionsList)
-    {
+    /**
+     * This method sets the versionsList variable to the given Set of PackageVersion objects.
+     * @param versionsList set of PackageVersions
+     */
+    public void setVersionsList(Set<PackageVersion> versionsList) {
         this.versionsList = versionsList;
     }
 
-    public Set<PackageVersion> getVersionsList()
-    {
+    /**
+     * This method returns the current versionList, which is a Set of PackageVersion objects.
+     * @return set of PackageVersions
+     */
+    public Set<PackageVersion> getVersionsList() {
         return versionsList;
     }
 
+    /**
+     * This method sets the ID variable to the given Long value.
+     * @param id Databank
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * This method returns the current ID, which is a Long integer.
+     * @return
+     */
     public Long getId() {
         return id;
     }
 
-
+    /**
+     * This method sets the gaString variable to the given String value.
+     * @param Ga
+     */
     public void setGaString(String Ga) {
         this.gaString = Ga;
     }
 
+    /**
+     * This method returns the current gaString, which is a String value.
+     * @return
+     */
     public String getGaString() {
         return gaString;
     }
 
-
+    /**
+     * This method returns the processed variable, which is a Boolean indicating whether or not the package has been processed.
+     * @return
+     */
     public Boolean getProcessed() {
         return processed;
     }
 
+    /**
+     * This method sets the processed variable to the given value, which should be a boolean value indicating whether or not the package has been processed.
+     * @param processed
+     */
     public void setProcessed(Boolean processed) {
         this.processed = processed;
     }
--- springBootCallAnalyse/src/main/java/de/skyfly/callanalyse/Entities/PackageVersion.java
+++ springBootCallAnalyse/src/main/java/de/skyfly/callanalyse/Entities/PackageVersion.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import org.hibernate.Length;
 import org.springframework.data.annotation.Id;
 
+/**  This class represents a package version in the database. */
 @Entity(name = "package_version")
 public class PackageVersion {
     @jakarta.persistence.Id
@@ -11,51 +12,70 @@ public class PackageVersion {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    // The GA string for this package version.
     @Column(length= Length.LONG32, name = "ga_string")
     private String gaString;
 
+    // The URL to download the jar file for this package version.
     @Column(unique = true, length= Length.LONG32)
     private String urlJar;
 
-
+    // The associated package name for this package version.
     @ManyToOne
     public PackageName packageName;
-
 
 
     public PackageVersion(PackageName packageName, String gaString, String urlJar) {
         this.packageName = packageName;
         this.gaString = gaString;
-        this.urlJar =urlJar;
-
+        this.urlJar = urlJar;
     }
-
+    
+    /**
+     * Default constructor for a new PackageVersion object.
+     */
     public PackageVersion() {
-
+    
     }
 
+    /**
+     * Set the id of the package version.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Get the id of the package version.
+     */
     public Long getId() {
         return id;
     }
 
-
+    /**
+     * Set the URL for downloading the package version's jar file.
+     */
     public void setUrlJar(String urlJar) {
         this.urlJar = urlJar;
     }
 
+    /**
+     * Get the URL for downloading the package version's jar file.
+     */
     public String getUrlJar() {
         return urlJar;
     }
 
-
+    /**
+     * Set the GaString of the package version.
+     */
     public void setGaString(String Ga) {
         this.gaString = Ga;
     }
 
+    /**
+     * Get the GaString of the package version.
+     */
     public String getGaString() {
         return gaString;
     }
